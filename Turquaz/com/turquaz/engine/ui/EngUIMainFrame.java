b998e3f0d3c052137d0215c10bc7d1cb994a475c
onsel <onsel>
2004-08-25 07:43:05 +0000

    *** empty log message ***

--- Turquaz/com/turquaz/engine/ui/EngUIMainFrame.java
+++ Turquaz/com/turquaz/engine/ui/EngUIMainFrame.java
@@ -14,7 +14,7 @@ import org.eclipse.swt.SWT;
 
 /**
 * @author  Onsel Armagan
-* @version  $id$
+* @version  $Id: EngUIMainFrame.java,v 1.4 2004/08/25 07:46:41 onsel Exp $
 */
 public class EngUIMainFrame extends org.eclipse.swt.widgets.Composite {
 	private MenuItem aboutMenuItem;
