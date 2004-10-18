3adb95deced283df30bc5f10e1a404f415d39c8e
onsel <onsel>
2004-10-18 09:47:34 +0000

    *** empty log message ***

--- Turquaz/com/turquaz/accounting/ui/AccUITransactionAdd.java
+++ Turquaz/com/turquaz/accounting/ui/AccUITransactionAdd.java
@@ -17,7 +17,7 @@ package com.turquaz.accounting.ui;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: AccUITransactionAdd.java,v 1.2 2004/10/17 17:46:48 onsel Exp $
+* @version  $Id: AccUITransactionAdd.java,v 1.3 2004/10/18 09:47:34 onsel Exp $
 */
 
 import org.eclipse.swt.layout.GridLayout;
@@ -26,6 +26,14 @@ import com.turquaz.engine.ui.component.DatePicker;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.SWT;
 
+
+/**
+* This code was generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a
+* for-profit company or business) then you should purchase
+* a license - please visit www.cloudgarden.com for details.
+*/
 public class AccUITransactionAdd extends org.eclipse.swt.widgets.Composite {
 
 	private DatePicker datePicker1;
