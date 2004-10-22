ee5b0f125c6c950e715cd9d312be4c89575690e4
cem <cem>
2004-10-22 12:36:00 +0000

    *** empty log message ***

--- Turquaz/com/turquaz/current/dal/CurDALCurrentCardSearch.java
+++ Turquaz/com/turquaz/current/dal/CurDALCurrentCardSearch.java
@@ -12,6 +12,8 @@ import net.sf.hibernate.Query;
 import net.sf.hibernate.Session;
 
 import com.turquaz.engine.dal.EngDALSessionFactory;
+import com.turquaz.engine.dal.TurqCurrency;
+import com.turquaz.engine.dal.TurqCurrentCardsGroup;
 import com.turquaz.engine.dal.TurqCurrentGroup;
 
 /**
