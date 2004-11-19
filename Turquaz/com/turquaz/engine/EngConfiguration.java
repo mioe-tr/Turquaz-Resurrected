fbfbec728300860be51db8fd1cc0f27ee9dfb388
onsel <onsel>
2004-11-19 08:16:56 +0000

    jar description added

--- Turquaz/com/turquaz/engine/EngConfiguration.java
+++ Turquaz/com/turquaz/engine/EngConfiguration.java
@@ -24,11 +24,12 @@ package com.turquaz.engine;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: EngConfiguration.java,v 1.4 2004/11/08 12:10:51 onsel Exp $
+* @version  $Id: EngConfiguration.java,v 1.5 2004/11/19 08:16:56 onsel Exp $
 */
-import java.util.Locale;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
+import java.io.FileInputStream;
+
+import java.util.Properties;
+
 
 /**
  * @author onsel
@@ -37,21 +38,37 @@ import java.util.ResourceBundle;
  * Window - Preferences - Java - Code Style - Code Templates
  */
 public class EngConfiguration {
-	private static final String BUNDLE_NAME = "config/turquaz";//$NON-NLS-1$
+	private static final String filename = "config/turquaz.properties";//$NON-NLS-1$
+    private Properties props ;
+	
 
-	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
-			.getBundle(BUNDLE_NAME,new Locale("",""));
-
+	private static EngConfiguration _instance;
 	private EngConfiguration() {
+	    try{
+     FileInputStream fis = new FileInputStream(filename);
+	   props = new Properties();
+	   props.load(fis);
+	    
+	    }
+	    catch(Exception ex){
+	        ex.printStackTrace();
+	    }
+	    
 	}
 
 	public static String getString(String key) {
-		// TODO Auto-generated method stub
-		try {
-			return RESOURCE_BUNDLE.getString(key);
-		} catch (MissingResourceException e) {
-			return null;
+		if(_instance==null){
+		    _instance = new EngConfiguration();
 		}
+	    
+	    return _instance.findString(key);
+	    
+	}
+	
+	private String findString(String Key){
+	
+	    return props.getProperty(Key);
+	    
 	}
 	
 }
