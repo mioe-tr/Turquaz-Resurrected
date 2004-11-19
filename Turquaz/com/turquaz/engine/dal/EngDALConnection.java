74fca60a46771182842f79ee227ec2a763236524
onsel <onsel>
2004-11-19 10:24:38 +0000

    *** empty log message ***

--- Turquaz/com/turquaz/engine/dal/EngDALConnection.java
+++ Turquaz/com/turquaz/engine/dal/EngDALConnection.java
@@ -22,7 +22,7 @@ import java.sql.*;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: EngDALConnection.java,v 1.8 2004/11/04 17:54:14 cem Exp $
+* @version  $Id: EngDALConnection.java,v 1.9 2004/11/19 10:24:38 onsel Exp $
 * 
 * Class for initial database configuration. It creates the necessary
 * bindings according to hibernate.cfg.xml
@@ -119,6 +119,29 @@ public class EngDALConnection {
   public void createTables()throws Exception{
   try{
   	stmt.execute(getGetCreateScript());
+  	String sql="";
+    // Open the file that is the first
+    // command line parameter
+   FileInputStream fstream = new FileInputStream("sql/seq.sql");
+                              // Convert our input stream to a
+                              // DataInputStream
+   DataInputStream in = new DataInputStream(fstream);
+
+    // Continue to read lines while
+    // there are still some left to read
+    while (in.available() !=0){
+   
+    	sql = in.readLine();
+    	if(sql.trim().startsWith("SELECT"));{
+    	    stmt.execute(sql);
+    	}
+     
+    }
+    in.close();
+    
+   
+     
+
   	
   	
   }
@@ -131,6 +154,8 @@ public class EngDALConnection {
   public Connection getCon() {
 	return this.con;
   }
+  
+  
   private String getGetCreateScript()throws Exception{
 
     try
