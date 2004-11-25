e26c341daccb1e26e7ce373978cf0b03aee58d47
cemdayanik <cemdayanik>
2004-11-25 19:09:14 +0000

    *** empty log message ***

--- Turquaz/com/turquaz/current/dal/CurDALCurrentCardAdd.java
+++ Turquaz/com/turquaz/current/dal/CurDALCurrentCardAdd.java
@@ -23,7 +23,7 @@ package com.turquaz.current.dal;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: CurDALCurrentCardAdd.java,v 1.4 2004/11/04 13:28:07 ehad Exp $
+* @version  $Id: CurDALCurrentCardAdd.java,v 1.5 2004/11/25 19:09:14 cemdayanik Exp $
 */
 import java.util.List;
 
@@ -33,6 +33,21 @@ import net.sf.hibernate.Transaction;
 
 import com.turquaz.engine.dal.EngDALSessionFactory;
 
+
+/**
+* This code was generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* *************************************
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED
+* for this machine, so Jigloo or this code cannot be used legally
+* for any corporate or commercial purpose.
+* *************************************
+*/
 /**
  * @author Ceday
  *
