3d1748d3795b3365d7759d79f8e8466a3f974622
cemdayanik <cemdayanik>
2004-12-03 19:12:03 +0000

    https://sourceforge.net/tracker/index.php?func=detail&aid=1076559&group_id=80174&atid=558950

--- Turquaz/com/turquaz/engine/ui/component/CurrencyText.java
+++ Turquaz/com/turquaz/engine/ui/component/CurrencyText.java
@@ -19,7 +19,7 @@ package com.turquaz.engine.ui.component;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: CurrencyText.java,v 1.5 2004/11/28 11:18:47 onsel Exp $
+* @version  $Id: CurrencyText.java,v 1.6 2004/12/03 19:12:03 cemdayanik Exp $
 */
 
 import java.math.BigDecimal;
@@ -107,7 +107,11 @@ public class CurrencyText extends Composite {
     String textcontrol = control.getText();
     e.doit = false;
     String newText = textcontrol.substring(0, e.start) + e.text + textcontrol.substring(e.end);
-    newText=newText.replaceAll(",","");
+    System.out.println(newText);
+    //newText=newText.replaceAll(",","");
+    newText=newText.replaceAll("\\.","");
+    newText=newText.replaceAll(",",".");
+    System.out.println(newText);
    /* if (e.keyCode == SWT.BS || e.keyCode == SWT.DEL){
     	e.doit=true;
     
@@ -130,23 +134,26 @@ public class CurrencyText extends Composite {
      e.doit=true;
      return;
     }
-   
+    System.out.println("beforevalid");
+    System.out.println(newText);
     Pattern realNumberPattern = Pattern.compile("-?[0-9]+[0-9]*(([" +decimalSymbol + "][0-9]?[0-9]?)|(["+decimalSymbol+"]))?");
     Matcher matcher = realNumberPattern.matcher(newText);
     boolean valid = matcher.matches();
     
 
     if (valid){
+    	System.out.println("valid");
     	text.removeVerifyListener(listener);
     	boolean isLastSeperator=(newText.toCharArray()[newText.length()-1]==decimalSymbol) ? true : false;
     	BigDecimal bd=new BigDecimal(newText);
     	TurquazDecimalFormat tdf=new TurquazDecimalFormat();
     	String formatted=tdf.format(bd);
     	if (isLastSeperator)
-    		formatted +=".";
+    		formatted +=",";
     	text.setText(formatted);
     	String s=textcontrol.substring(0,e.start)+e.text;
-    	s=s.replaceAll(",","");
+        s=s.replaceAll("\\.","");
+        s=s.replaceAll(",",".");
     	int index=newText.indexOf(".");
     	int diff=0;
     	if (index==-1)
@@ -183,7 +190,8 @@ public class CurrencyText extends Composite {
  }
  public String getText(){
  	String formatted=text.getText(); 	
- 	return formatted.replaceAll(",","");
+ 	formatted=formatted.replace('.','\0');
+ 	return formatted.replace(',','.');
  }
  public void selectAll(){
  	text.selectAll();
