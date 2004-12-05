41150f2db9f9c0788e9d53bcc28b532d12d3ba79
onsel <onsel>
2004-12-05 08:59:36 +0000

    choose modul added to main frame..

--- Turquaz/com/turquaz/engine/dal/EngDALSessionFactory.java
+++ Turquaz/com/turquaz/engine/dal/EngDALSessionFactory.java
@@ -30,7 +30,7 @@ import net.sf.hibernate.cfg.Configuration;
 /**
  * @author onsel
  *
- * @version $Id: EngDALSessionFactory.java,v 1.15 2004/12/02 08:25:46 onsel Exp $
+ * @version $Id: EngDALSessionFactory.java,v 1.16 2004/12/05 08:59:36 onsel Exp $
  * 
  * Class for initial database configuration. It creates the necessary
  * bindings according to hibernate.cfg.xml
@@ -81,9 +81,10 @@ public class EngDALSessionFactory {
 		props.put("hibernate.connection.driver_class",driver);
 		props.put("hibernate.connection.username",username);
 		props.put("hibernate.connection.password",password);
-		props.put("hibernate.show_sql","true");
+		props.put("hibernate.show_sql","false");
 		props.put("hibernate.dialect","net.sf.hibernate.dialect.PostgreSQLDialect");
 		props.put("hibernate.schema","public");
+		
 			
 		cfg = cfg.configure();
 		cfg.addProperties(props);
