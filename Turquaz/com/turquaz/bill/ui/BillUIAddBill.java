7cea85a9660a27c68de87fcfed94d5e7764f3b38
onsel <onsel>
2005-01-07 10:30:07 +0000

    due days added to current cards. minor bug fix for bill update...

--- Turquaz/com/turquaz/bill/ui/BillUIAddBill.java
+++ Turquaz/com/turquaz/bill/ui/BillUIAddBill.java
@@ -18,7 +18,7 @@ package com.turquaz.bill.ui;
 
 /**
  * @author  Onsel Armagan
- * @version  $Id: BillUIAddBill.java,v 1.37 2005/01/03 00:00:04 onsel Exp $
+ * @version  $Id: BillUIAddBill.java,v 1.38 2005/01/07 10:30:07 onsel Exp $
  */
 
 import java.math.BigDecimal;
@@ -497,8 +497,8 @@ public class BillUIAddBill extends Composite
                                 GridData txtCurrentCardLData = new GridData();
                                 
                                 txtCurrentCard.setBackground(SWTResourceManager.getColor(255,255,255));
-                                txtCurrentCardLData.widthHint = 213;
-                                txtCurrentCardLData.heightHint = 13;
+                                txtCurrentCardLData.widthHint = 210;
+                                txtCurrentCardLData.heightHint = 17;
                                 txtCurrentCard.setLayoutData(txtCurrentCardLData);
                             }
                             {
@@ -534,9 +534,8 @@ public class BillUIAddBill extends Composite
                                     SWT.NONE);
                                 GridData txtBillDocumentNoLData = new GridData();
                                 txtBillDocumentNoLData.widthHint = 133;
-                                txtBillDocumentNoLData.heightHint = 15;
-                                txtConsignmentDocumentNo
-                                    .setLayoutData(txtBillDocumentNoLData);
+                                txtBillDocumentNoLData.heightHint = 18;
+                                txtConsignmentDocumentNo.setLayoutData(txtBillDocumentNoLData);
                             }
                             {
                                 lblDate = new CLabel(compInfoPanel, SWT.LEFT);
