66183914e9c6e67ea0b66b38fad5ff2613ca05bf
onsel <onsel>
2005-01-10 20:25:26 +0000

    *** empty log message ***

--- Turquaz/com/turquaz/bill/ui/BillUIAddSellBill.java
+++ Turquaz/com/turquaz/bill/ui/BillUIAddSellBill.java
@@ -18,7 +18,7 @@ package com.turquaz.bill.ui;
 
 /**
  * @author  Huseyin Ergun
- * @version  $Id: BillUIAddSellBill.java,v 1.34 2005/01/10 20:18:05 onsel Exp $
+ * @version  $Id: BillUIAddSellBill.java,v 1.35 2005/01/10 20:25:26 onsel Exp $
  */
 
 import java.math.BigDecimal;
@@ -639,7 +639,7 @@ public class BillUIAddSellBill extends Composite
 							}
                             {
                                 lblDueDate = new CLabel(compInfoPanel, SWT.NONE);
-                                lblDueDate.setText("Vade Tarihi");
+                                lblDueDate.setText(Messages.getString("BillUIAddSellBill.18")); //$NON-NLS-1$
                             }
                             {
                                 dateDueDate = new DatePicker(
@@ -1335,7 +1335,7 @@ public class BillUIAddSellBill extends Composite
 				if (Now< Min)
 				{
 					MessageBox msg=new MessageBox(this.getShell(), SWT.ICON_WARNING);
-					msg.setMessage("Uyar?: Stok kart?n?n minimum miktar?n alt?na iniyorsunuz!");
+					msg.setMessage(Messages.getString("BillUIAddSellBill.19")); //$NON-NLS-1$
 					msg.open();
 				}
 			
