4abe0a981570dc63d0f89d634e8a2ed13086a5b2
cemdayanik <cemdayanik>
2005-01-13 15:20:38 +0000

    added CurUICurrentCardVoucher

--- Turquaz/com/turquaz/engine/ui/component/TreeFactory.java
+++ Turquaz/com/turquaz/engine/ui/component/TreeFactory.java
@@ -19,7 +19,7 @@ package com.turquaz.engine.ui.component;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: TreeFactory.java,v 1.59 2005/01/13 13:34:36 onsel Exp $
+* @version  $Id: TreeFactory.java,v 1.60 2005/01/13 15:20:38 cemdayanik Exp $
 */
 import org.eclipse.swt.SWT;
 
@@ -64,6 +64,7 @@ import com.turquaz.consignment.ui.ConUIConsignmentSearch;
 import com.turquaz.current.ui.CurUICurrentCardAbstract;
 import com.turquaz.current.ui.CurUICurrentCardAdd;
 import com.turquaz.current.ui.CurUICurrentCardSearch;
+import com.turquaz.current.ui.CurUICurrentCardVoucher;
 import com.turquaz.current.ui.CurUIInitialTransaction;
 import com.turquaz.current.ui.CurUITransactionSearch;
 import com.turquaz.engine.Messages;
@@ -287,14 +288,16 @@ public final class TreeFactory {
 		}
 		
 		if(EngBLPermissions.getPermission(CurUIInitialTransaction.class.getName())>0){
-			item = new TreeItem(root,SWT.NULL);
-			
-			item.setText(Messages.getString("TreeFactory.55"));  //$NON-NLS-1$
-			
-			item.setData(CurUIInitialTransaction.class.getName());
-		
+			item = new TreeItem(root,SWT.NULL);			
+			item.setText(Messages.getString("TreeFactory.55"));  //$NON-NLS-1$			
+			item.setData(CurUIInitialTransaction.class.getName());		
 		}
 		
+		if(EngBLPermissions.getPermission(CurUICurrentCardVoucher.class.getName())>0){
+			item = new TreeItem(root,SWT.NULL);			
+			item.setText(Messages.getString("TreeFactory.58")); //$NON-NLS-1$
+			item.setData(CurUICurrentCardVoucher.class.getName());		
+		}
 		root.setExpanded(true);
 		return tree;
 	}
