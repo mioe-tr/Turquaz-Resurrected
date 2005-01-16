ad2cc25780c1c54378b1c3115e54a768c49903c7
cemdayanik <cemdayanik>
2005-01-16 16:30:04 +0000

    [ 1102992 ] inventory level warning

--- Turquaz/com/turquaz/bill/ui/BillUIBillFromConsignment.java
+++ Turquaz/com/turquaz/bill/ui/BillUIBillFromConsignment.java
@@ -18,7 +18,7 @@ package com.turquaz.bill.ui;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: BillUIBillFromConsignment.java,v 1.27 2005/01/13 16:22:53 onsel Exp $
+* @version  $Id: BillUIBillFromConsignment.java,v 1.28 2005/01/16 16:30:05 cemdayanik Exp $
 */
 
 import java.math.BigDecimal;
@@ -991,20 +991,23 @@ implements SecureComposite{
 		}
 		
 	}
-	public void save(){
+	public void save()
+	{
 		MessageBox msg = new MessageBox(this.getShell(),SWT.NULL);
-	try{	
-		if(verifyFields()){
+		try
+		{	
+			if(verifyFields())
+			{
 			
-	        int type =0;
-		if(comboConsignmentType.getText().equals(Messages.getString("BillUIBillFromConsignment.33"))){ //$NON-NLS-1$
-			type =1;
-		}
-	     TurqConsignment consignment = (TurqConsignment)txtConsignment.getData();
+				int type =0;
+				if(comboConsignmentType.getText().equals(Messages.getString("BillUIBillFromConsignment.33"))){ //$NON-NLS-1$
+					type =1;
+				}
+				TurqConsignment consignment = (TurqConsignment)txtConsignment.getData();
 	     
-	     Boolean paymentType = (Boolean)comboPaymentType.getData(comboPaymentType.getText());
+				Boolean paymentType = (Boolean)comboPaymentType.getData(comboPaymentType.getText());
 	     
-		TurqBill bill =blAddBill.saveBill(txtDocumentNo.getText(),
+				TurqBill bill =blAddBill.saveBill(txtDocumentNo.getText(),
 										txtDefinition.getText(),
 										false,
 										dateBillDate.getDate(),
@@ -1014,17 +1017,18 @@ implements SecureComposite{
 										paymentType.booleanValue() ? accountPickerCurAcc.getData() : null,
 										 dateDueDate.getDate());
 		
-		saveGroups(bill.getBillsId());
-		msg.setMessage(Messages.getString("BillUIBillFromConsignment.34")); //$NON-NLS-1$
-		msg.open();
-		newForm();
+				saveGroups(bill.getBillsId());
+				msg.setMessage(Messages.getString("BillUIBillFromConsignment.34")); //$NON-NLS-1$
+				msg.open();
+				newForm();
+			}
 		}
-	}
-	catch(Exception ex){
-		ex.printStackTrace();
-		msg.setMessage(ex.getMessage());
-		msg.open();
-	}
+		catch(Exception ex)
+		{
+			ex.printStackTrace();
+			msg.setMessage(ex.getMessage());
+			msg.open();
+		}
 		
 	}
 	public void delete(){
--- Turquaz/com/turquaz/engine/dal/EngDALSessionFactory.java
+++ Turquaz/com/turquaz/engine/dal/EngDALSessionFactory.java
@@ -30,7 +30,7 @@ import net.sf.hibernate.cfg.Configuration;
 /**
  * @author onsel
  *
- * @version $Id: EngDALSessionFactory.java,v 1.28 2005/01/16 13:21:18 onsel Exp $
+ * @version $Id: EngDALSessionFactory.java,v 1.29 2005/01/16 16:30:05 cemdayanik Exp $
  * 
  * Class for initial database configuration. It creates the necessary
  * bindings according to hibernate.cfg.xml
@@ -86,7 +86,7 @@ public class EngDALSessionFactory {
 		props.put("hibernate.connection.driver_class",driver);
 		props.put("hibernate.connection.username",username);
 		props.put("hibernate.connection.password",password);
-		props.put("hibernate.show_sql","true");
+		props.put("hibernate.show_sql","false");
 		props.put("hibernate.schema","public");
 		
 			
