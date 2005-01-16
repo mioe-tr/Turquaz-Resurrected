6fe19bec098d993d55f3218644961eb6bffc65ed
onsel <onsel>
2005-01-16 17:00:47 +0000

    Shell names added to admin and bank update dialogs...

--- Turquaz/com/turquaz/bank/Messages.java
+++ Turquaz/com/turquaz/bank/Messages.java
@@ -19,7 +19,7 @@ package com.turquaz.bank;
 
 /**
 * @author  Huseyin Ergun
-* @version  $Id: Messages.java,v 1.6 2004/12/23 15:49:55 onsel Exp $
+* @version  $Id: Messages.java,v 1.7 2005/01/16 17:00:47 onsel Exp $
 */
 
 
@@ -28,7 +28,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "com.turquaz.bank.messages";//$NON-NLS-1$
+	private static final String BUNDLE_NAME = "com.turquaz.bank.messages_tr_TR";//$NON-NLS-1$
 
 
 	private Messages() {
