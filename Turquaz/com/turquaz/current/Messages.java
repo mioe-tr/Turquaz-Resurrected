70195afd82c56fa357b3e7d3e7614bb9233ccdcc
cemdayanik <cemdayanik>
2005-02-03 10:03:20 +0000

    added currentDate to all reports..

--- Turquaz/com/turquaz/current/Messages.java
+++ Turquaz/com/turquaz/current/Messages.java
@@ -18,7 +18,7 @@ package com.turquaz.current;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: Messages.java,v 1.14 2005/01/26 21:21:03 huseyiner Exp $
+* @version  $Id: Messages.java,v 1.15 2005/02/03 10:03:22 cemdayanik Exp $
 */
 
 import java.util.Locale;
@@ -26,7 +26,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-	private static final String BUNDLE_NAME = "com.turquaz.current.messages";//$NON-NLS-1$
+	private static final String BUNDLE_NAME = "com.turquaz.current.messages_tr_TR";//$NON-NLS-1$
 
 	private Messages() {
 	}
