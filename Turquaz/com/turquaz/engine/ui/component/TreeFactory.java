62e7d756afb7cfd8ef81542d58aa4b399c7b75d6
onsel <onsel>
2005-02-04 17:44:29 +0000

    transfer between banks added to program menu

--- Turquaz/com/turquaz/engine/ui/component/TreeFactory.java
+++ Turquaz/com/turquaz/engine/ui/component/TreeFactory.java
@@ -19,7 +19,7 @@ package com.turquaz.engine.ui.component;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: TreeFactory.java,v 1.75 2005/02/04 16:09:12 cemdayanik Exp $
+* @version  $Id: TreeFactory.java,v 1.76 2005/02/04 17:44:29 onsel Exp $
 */
 import org.eclipse.swt.SWT;
 
@@ -59,6 +59,7 @@ import com.turquaz.bank.ui.BankUIMoneyTransferOut;
 import com.turquaz.bank.ui.BankUIOtherTransIn;
 import com.turquaz.bank.ui.BankUIOtherTransOut;
 import com.turquaz.bank.ui.BankUISearchMoneyTransaction;
+import com.turquaz.bank.ui.BankUITransferBetweenAccounts;
 import com.turquaz.bill.ui.BillUIAddBuyBill;
 import com.turquaz.bill.ui.BillUIAddSellBill;
 import com.turquaz.bill.ui.BillUIBillFromConsignment;
@@ -234,6 +235,11 @@ public final class TreeFactory {
 			item = new TreeItem(bankTrans,SWT.NULL);
 			item.setText(Messages.getString("TreeFactory.72"));  //$NON-NLS-1$
 			item.setData(BankUIOtherTransOut.class.getName());
+		}
+		if(EngBLPermissions.getPermission(BankUITransferBetweenAccounts.class.getName())>0){
+			item = new TreeItem(bankTrans,SWT.NULL);
+			item.setText(Messages.getString("TreeFactory.76"));   //$NON-NLS-1$
+			item.setData(BankUITransferBetweenAccounts.class.getName());
 		}
 		if(EngBLPermissions.getPermission(BankUIInitialTransaction.class.getName())>0){
 			item = new TreeItem(bankTrans,SWT.NULL);
