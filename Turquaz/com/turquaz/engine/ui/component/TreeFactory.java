2b9dd0186abaa0d8482f754c7913bd2fb2f92e36
cemdayanik <cemdayanik>
2005-02-07 12:10:42 +0000

    externalize strings..

--- Turquaz/com/turquaz/engine/ui/component/TreeFactory.java
+++ Turquaz/com/turquaz/engine/ui/component/TreeFactory.java
@@ -19,7 +19,7 @@ package com.turquaz.engine.ui.component;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: TreeFactory.java,v 1.81 2005/02/07 11:27:23 onsel Exp $
+* @version  $Id: TreeFactory.java,v 1.82 2005/02/07 12:10:42 cemdayanik Exp $
 */
 import org.eclipse.swt.SWT;
 
@@ -89,6 +89,7 @@ import com.turquaz.inventory.ui.InvUICardAdd;
 import com.turquaz.inventory.ui.InvUICardSearch;
 
 import com.turquaz.inventory.ui.InvUIGroupingPlan;
+import com.turquaz.inventory.ui.InvUIInventoryCardAbstract;
 import com.turquaz.inventory.ui.InvUIInventoryLedger;
 import com.turquaz.inventory.ui.InvUIInventoryTransactionReport;
 import com.turquaz.inventory.ui.InvUIProfitAnalysis;
@@ -184,13 +185,19 @@ public final class TreeFactory {
 			item.setData(InvUITransactionsTotalReport.class.getName());
 			}
 		
+		if(EngBLPermissions.getPermission(InvUIInventoryCardAbstract.class.getName())>0){
+			item = new TreeItem(reports,SWT.NULL);
+			item.setText(Messages.getString("TreeFactory.97")); //$NON-NLS-1$
+			item.setData(InvUIInventoryCardAbstract.class.getName());
+			}
+		
 		TreeItem adminRoot = new TreeItem(tree,SWT.NULL);
 		adminRoot.setText(Messages.getString("TreeFactory.9"));   //$NON-NLS-1$
 		
 		
 		if(EngBLPermissions.getPermission(InvUIGroupingPlan.class.getName())>0){
 			item = new TreeItem(adminRoot,SWT.NULL);
-			item.setText("Stok Gruplar?"); 
+			item.setText(Messages.getString("TreeFactory.98"));  //$NON-NLS-1$
 			item.setData(InvUIGroupingPlan.class.getName());
 			}
 
