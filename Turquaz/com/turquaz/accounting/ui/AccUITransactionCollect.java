0ba5232d723f918e1773fd6f2feea1974078603b
cemdayanik <cemdayanik>
2005-02-21 16:47:45 +0000

    some fields' name are changed..

--- Turquaz/com/turquaz/accounting/ui/AccUITransactionCollect.java
+++ Turquaz/com/turquaz/accounting/ui/AccUITransactionCollect.java
@@ -18,7 +18,7 @@ package com.turquaz.accounting.ui;
 
 /**
  * @author  Onsel Armagan
- * @version  $Id: AccUITransactionCollect.java,v 1.41 2005/02/18 18:53:04 onsel Exp $
+ * @version  $Id: AccUITransactionCollect.java,v 1.42 2005/02/21 16:47:47 cemdayanik Exp $
  */
 
 import java.math.BigDecimal;
@@ -180,8 +180,7 @@ public class AccUITransactionCollect extends Composite implements
 			this.setLayout(thisLayout);
 			{
 				lbldocumentNo = new CLabel(this, SWT.NONE);
-				lbldocumentNo.setText(Messages
-						.getString("AccUITransactionCollect.0")); //$NON-NLS-1$
+				lbldocumentNo.setText(Messages.getString("AccUITransactionCollect.0")); //$NON-NLS-1$
 				GridData lbldocumentNoLData = new GridData();
 				lbldocumentNoLData.verticalAlignment = GridData.BEGINNING;
 				lbldocumentNoLData.widthHint = 79;
@@ -254,7 +253,7 @@ public class AccUITransactionCollect extends Composite implements
 			}
 			//START >>  lblCurrency
 			lblCurrency = new CLabel(this, SWT.NONE);
-			lblCurrency.setText("Para Birimi");
+			lblCurrency.setText(Messages.getString("AccUITransactionCollect.8")); //$NON-NLS-1$
 			//END <<  lblCurrency
 			//START >>  comboCurrencyType
 			comboCurrencyType = new CCombo(this, SWT.NONE);
@@ -490,7 +489,7 @@ public class AccUITransactionCollect extends Composite implements
 			} 
 			else if ((exchangeCurrency=(TurqCurrency)comboCurrencyType.getData(comboCurrencyType.getText()))==null)
 			{
-				msg.setMessage("Para birimi seçmelisiniz!");
+				msg.setMessage(Messages.getString("AccUITransactionCollect.10")); //$NON-NLS-1$
 				msg.open();
 				comboCurrencyType.setFocus();
 				return false;
@@ -499,7 +498,7 @@ public class AccUITransactionCollect extends Composite implements
 			{
 					if ((exchangeRatio=AccBLTransactionSearch.getExchangeRatio(baseCurrency,exchangeCurrency,Calendar.getInstance().getTime()))==null)
 					{
-						msg.setMessage("Bugün için çapraz kur tan?mlamal?s?n?z!");
+						msg.setMessage(Messages.getString("AccUITransactionCollect.11")); //$NON-NLS-1$
 						msg.open();
 						return false;	
 				
--- Turquaz/com/turquaz/accounting/ui/AccUITransactionPayment.java
+++ Turquaz/com/turquaz/accounting/ui/AccUITransactionPayment.java
@@ -17,7 +17,7 @@ package com.turquaz.accounting.ui;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: AccUITransactionPayment.java,v 1.37 2005/02/18 18:53:03 onsel Exp $
+* @version  $Id: AccUITransactionPayment.java,v 1.38 2005/02/21 16:47:47 cemdayanik Exp $
 */
 
 import java.math.BigDecimal;
@@ -166,8 +166,7 @@ public class AccUITransactionPayment extends Composite implements SecureComposit
 			this.setLayout(thisLayout);
 			{
 				lbldocumentNo = new CLabel(this, SWT.NONE);
-				lbldocumentNo.setText(Messages
-					.getString("AccUITransactionPayment.0")); //$NON-NLS-1$
+				lbldocumentNo.setText(Messages.getString("AccUITransactionPayment.0")); //$NON-NLS-1$
 				lbldocumentNo
 					.setSize(new org.eclipse.swt.graphics.Point(93, 18));
 				GridData lbldocumentNoLData = new GridData();
@@ -237,7 +236,7 @@ public class AccUITransactionPayment extends Composite implements SecureComposit
 			}
 			//START >>  lblCurrency
 			lblCurrency = new CLabel(this, SWT.NONE);
-			lblCurrency.setText("Para Birimi");
+			lblCurrency.setText(Messages.getString("AccUITransactionPayment.5")); //$NON-NLS-1$
 			//END <<  lblCurrency
 			//START >>  comboCurrencyType
 			comboCurrencyType = new CCombo(this, SWT.NONE);
@@ -411,7 +410,7 @@ public class AccUITransactionPayment extends Composite implements SecureComposit
    			}
    			else if ((exchangeCurrency=(TurqCurrency)comboCurrencyType.getData(comboCurrencyType.getText()))==null)
    			{
-   				msg.setMessage("Para birimi seçmelisiniz!");
+   				msg.setMessage(Messages.getString("AccUITransactionPayment.6")); //$NON-NLS-1$
    				msg.open();
 				comboCurrencyType.setFocus();
 				return false;
@@ -420,7 +419,7 @@ public class AccUITransactionPayment extends Composite implements SecureComposit
    			{
    				if ((exchangeRatio=AccBLTransactionSearch.getExchangeRatio(baseCurrency,exchangeCurrency,Calendar.getInstance().getTime()))==null)
 				{
-					msg.setMessage("Bugün için çapraz kur tan?mlamal?s?n?z!");
+					msg.setMessage(Messages.getString("AccUITransactionPayment.7")); //$NON-NLS-1$
 					msg.open();
 					return false;	
 			
