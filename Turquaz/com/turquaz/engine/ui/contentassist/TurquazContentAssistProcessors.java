2ddb5fe69da98d1f5d2ab9f15eef9d5cf6317cde
onsel <onsel>
2005-02-24 08:38:04 +0000

    content assistan is now case sensitive...

--- Turquaz/com/turquaz/engine/ui/contentassist/TurquazContentAssistProcessors.java
+++ Turquaz/com/turquaz/engine/ui/contentassist/TurquazContentAssistProcessors.java
@@ -17,11 +17,12 @@ package com.turquaz.engine.ui.contentassist;
 
 /**
 * @author  Onsel
-* @version  $Id: TurquazContentAssistProcessors.java,v 1.24 2005/02/07 10:40:32 onsel Exp $
+* @version  $Id: TurquazContentAssistProcessors.java,v 1.25 2005/02/24 08:38:04 onsel Exp $
 */
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import org.eclipse.jface.contentassist.IContentAssistSubjectControl;
 import org.eclipse.jface.contentassist.ISubjectControlContentAssistProcessor;
@@ -279,17 +280,21 @@ public class TurquazContentAssistProcessors implements
             int documentOffset, List propList) {
         int qlen = qualifier.length();
 
+        qualifier = qualifier.toLowerCase(Locale.getDefault());
         // Loop through all proposals
         for (int i = 0; i < proposedCodes.length; i++) {
-           if(propList.size()>100){
+           if(propList.size()>1000){
                return;
            }
             String startTag = proposedCodes[i].text;
             String info = proposedCodes[i].info;
-
+           
+             
             String text = startTag;
+            String lower_text = text.toLowerCase(Locale.getDefault());
+           
             // Yes -- compute whole proposal text
-            if (text.startsWith(qualifier)) {
+            if (lower_text.startsWith(qualifier)) {
 
                 // Derive cursor position
                 int cursor = startTag.length();
