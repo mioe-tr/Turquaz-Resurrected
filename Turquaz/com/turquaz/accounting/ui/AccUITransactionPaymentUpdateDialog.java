2de9a4bc817fb9a4959565d134d016d5a75a6adc
cemdayanik <cemdayanik>
2005-03-01 20:02:01 +0000

    HibernateComparer added..Transaction will be ordered as they have been saved in the update screens..

--- Turquaz/com/turquaz/accounting/ui/AccUITransactionPaymentUpdateDialog.java
+++ Turquaz/com/turquaz/accounting/ui/AccUITransactionPaymentUpdateDialog.java
@@ -17,13 +17,16 @@ package com.turquaz.accounting.ui;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: AccUITransactionPaymentUpdateDialog.java,v 1.23 2005/03/01 16:24:38 onsel Exp $
+* @version  $Id: AccUITransactionPaymentUpdateDialog.java,v 1.24 2005/03/01 20:02:01 cemdayanik Exp $
 */
 
 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.eclipse.swt.graphics.Rectangle;
@@ -44,6 +47,7 @@ import org.eclipse.swt.events.DisposeListener;
 import com.turquaz.accounting.Messages;
 import com.turquaz.accounting.bl.AccBLTransactionUpdate;
 import com.turquaz.accounting.ui.AccUITransactionPayment;
+import com.turquaz.engine.bl.EngBLHibernateComparer;
 import com.turquaz.engine.bl.EngBLPermissions;
 import com.turquaz.engine.dal.TurqAccountingTransaction;
 import com.turquaz.engine.dal.TurqAccountingTransactionColumn;
@@ -258,12 +262,16 @@ public class AccUITransactionPaymentUpdateDialog extends org.eclipse.swt.widgets
 	    
 	Set transactionRows = accTrans.getTurqAccountingTransactionColumns();
 	
-	Iterator it = transactionRows.iterator();
+	List transRows=new ArrayList();
+	transRows.addAll(transactionRows);
+	Collections.sort(transRows,new EngBLHibernateComparer());
+	
 	TurqAccountingTransactionColumn transRow;
 	TableItem item;
-	while(it.hasNext()){
+	for (int k=0; k<transRows.size(); k++)
+	{
 	
-	transRow =(TurqAccountingTransactionColumn)it.next();
+		transRow=(TurqAccountingTransactionColumn)transRows.get(k);
 	
 	if(!transRow.getDeptAmount().toString().equals("0")){ //$NON-NLS-1$
 	    ITableRow row = new AccUITransactionPaymentTableRow(compTransactionPayment.rowList);
--- Turquaz/com/turquaz/accounting/ui/AccUITransactionUpdateDialog.java
+++ Turquaz/com/turquaz/accounting/ui/AccUITransactionUpdateDialog.java
@@ -17,12 +17,15 @@ package com.turquaz.accounting.ui;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: AccUITransactionUpdateDialog.java,v 1.33 2005/03/01 16:24:38 onsel Exp $
+* @version  $Id: AccUITransactionUpdateDialog.java,v 1.34 2005/03/01 20:02:02 cemdayanik Exp $
 */
 
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 
 import java.util.Set;
 
@@ -41,9 +44,11 @@ import org.eclipse.swt.widgets.CoolItem;
 import com.cloudgarden.resource.SWTResourceManager;
 import org.eclipse.swt.widgets.CoolBar;
 import org.eclipse.swt.layout.GridData;
+
 import com.turquaz.accounting.Messages;
 import com.turquaz.accounting.bl.AccBLTransactionUpdate;
 import com.turquaz.accounting.ui.AccUITransactionAdd;
+import com.turquaz.engine.bl.EngBLHibernateComparer;
 import com.turquaz.engine.bl.EngBLPermissions;
 import com.turquaz.engine.bl.EngBLUtils;
 import com.turquaz.engine.dal.TurqAccountingTransaction;
@@ -278,18 +283,15 @@ public void showDialog(TurqAccountingTransaction accTrans){
 	compTransactionAdd.rowList.removeAll();
 		
 	Set transactionRows = accTrans.getTurqAccountingTransactionColumns();
-	//List transRows=new ArrayList();
-	//transRows.addAll(transactionRows);
-	//Collections.sort(transRows);
-	Iterator it = transactionRows.iterator();
+	List transRows=new ArrayList();
+	transRows.addAll(transactionRows);
+	Collections.sort(transRows,new EngBLHibernateComparer());
 	TurqAccountingTransactionColumn transRow;
 	TableItem item;
 	boolean isCurrencySet = false;
-	while(it.hasNext())
-	//for (int k=0; k<transRows.size(); k++)
+	for (int k=0; k<transRows.size(); k++)
 	{
-		transRow =(TurqAccountingTransactionColumn)it.next();
-		//transRow=(TurqAccountingTransactionColumn)transRows.get(k);
+		transRow=(TurqAccountingTransactionColumn)transRows.get(k);
 	
 		ITableRow row = new AccUITransactionAddTableRow(compTransactionAdd.rowList);
 		row.setDBObject(transRow);
