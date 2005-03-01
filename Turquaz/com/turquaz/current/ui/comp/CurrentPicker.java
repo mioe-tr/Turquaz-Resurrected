4bebb5f3f4572e2c13ea007929f7e7eca50a3a12
onsel <onsel>
2005-03-01 18:26:59 +0000

    cheque dal functions updated...

--- Turquaz/com/turquaz/current/ui/comp/CurrentPicker.java
+++ Turquaz/com/turquaz/current/ui/comp/CurrentPicker.java
@@ -17,7 +17,7 @@ package com.turquaz.current.ui.comp;
 
 /**
 * @author  Onsel Armagan
-* @version  $Id: CurrentPicker.java,v 1.9 2005/02/28 19:25:28 onsel Exp $
+* @version  $Id: CurrentPicker.java,v 1.10 2005/03/01 18:27:00 onsel Exp $
 */
 
 import org.eclipse.jface.contentassist.TextContentAssistSubjectAdapter;
@@ -176,7 +176,9 @@ public class CurrentPicker extends org.eclipse.swt.widgets.Composite {
 		if(obj==null)
 		{
 		    text1.setBackground(SWTResourceManager.getColor(255,150, 150));
-		    accountPicker.setData(null);
+		    
+		    if(accountPicker!=null)
+		    	accountPicker.setData(null);
 		}
 		else
 		{
