97e22d445bd82aa95f57efeab12b1276535416e1
onsel <onsel>
2005-04-25 11:54:29 +0000

    bill module log utility changed...

--- Turquaz/com/turquaz/bill/ui/BillUIAddBuyBill.java
+++ Turquaz/com/turquaz/bill/ui/BillUIAddBuyBill.java
@@ -17,14 +17,13 @@ package com.turquaz.bill.ui;
 /************************************************************************/
 /**
  * @author  Huseyin Ergun
- * @version  $Id: BillUIAddBuyBill.java,v 1.76 2005/04/24 14:53:56 cemdayanik Exp $
+ * @version  $Id: BillUIAddBuyBill.java,v 1.77 2005/04/25 11:54:29 onsel Exp $
  */
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Vector;
-import org.apache.log4j.Logger;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.layout.GridData;
@@ -61,6 +60,7 @@ import com.turquaz.bill.bl.BillBLAddGroups;
 import com.turquaz.consignment.ConsKeys;
 import com.turquaz.consignment.bl.ConBLAddConsignment;
 import com.turquaz.engine.bl.EngBLCommon;
+import com.turquaz.engine.bl.EngBLLogger;
 import com.turquaz.engine.dal.TurqBill;
 import com.turquaz.engine.dal.TurqBillGroup;
 import com.turquaz.engine.dal.TurqCurrency;
@@ -690,7 +690,7 @@ public class BillUIAddBuyBill extends Composite implements SecureComposite
 		}
 		catch (Exception e)
 		{
-			e.printStackTrace();
+            EngBLLogger.log(this.getClass(),e,getShell());
 		}
 	}
 
@@ -711,9 +711,7 @@ public class BillUIAddBuyBill extends Composite implements SecureComposite
 		}
 		catch (Exception ex)
 		{
-			Logger loger = Logger.getLogger(this.getClass());
-			loger.error("Exception Caught", ex); //$NON-NLS-1$
-			ex.printStackTrace();
+            EngBLLogger.log(this.getClass(),ex,getShell());
 		}
 	}
 
@@ -743,9 +741,7 @@ public class BillUIAddBuyBill extends Composite implements SecureComposite
 		}
 		catch (Exception ex)
 		{
-			Logger loger = Logger.getLogger(this.getClass());
-			loger.error("Exception Caught", ex); //$NON-NLS-1$
-			ex.printStackTrace();
+            EngBLLogger.log(this.getClass(),ex,getShell());
 		}
 	}
 
@@ -780,10 +776,8 @@ public class BillUIAddBuyBill extends Composite implements SecureComposite
 		}
 		catch (Exception ex)
 		{
-			Logger loger = Logger.getLogger(this.getClass());
-			loger.error("Exception Caught", ex); //$NON-NLS-1$
-			ex.printStackTrace();
-		}
+            EngBLLogger.log(this.getClass(),ex,getShell());
+        }
 	}
 
 	public void postInitGui()
@@ -821,9 +815,7 @@ public class BillUIAddBuyBill extends Composite implements SecureComposite
 		}
 		catch (Exception ex)
 		{
-			Logger loger = Logger.getLogger(this.getClass());
-			loger.error("Exception Caught", ex); //$NON-NLS-1$
-			ex.printStackTrace();
+            EngBLLogger.log(this.getClass(),ex,getShell());
 		}
 	}
 
@@ -1007,9 +999,7 @@ public class BillUIAddBuyBill extends Composite implements SecureComposite
 		}
 		catch (Exception ex)
 		{
-			Logger loger = Logger.getLogger(this.getClass());
-			loger.error("Exception Caught", ex); //$NON-NLS-1$
-			ex.printStackTrace();
+            EngBLLogger.log(this.getClass(),ex,getShell());
 			return false;
 		}
 	}
@@ -1049,9 +1039,7 @@ public class BillUIAddBuyBill extends Composite implements SecureComposite
 		}
 		catch (Exception ex)
 		{
-			Logger loger = Logger.getLogger(this.getClass());
-			loger.error("Exception Caught", ex); //$NON-NLS-1$
-			ex.printStackTrace();
+            EngBLLogger.log(this.getClass(),ex,getShell());
 			return false;
 		}
 	}
@@ -1112,9 +1100,7 @@ public class BillUIAddBuyBill extends Composite implements SecureComposite
 		}
 		catch (Exception ex)
 		{
-			Logger loger = Logger.getLogger(this.getClass());
-			loger.error("Exception Caught", ex); //$NON-NLS-1$
-			ex.printStackTrace();
+            EngBLLogger.log(this.getClass(),ex,getShell());
 		}
 	}
 
--- Turquaz/com/turquaz/bill/ui/BillUIBillFromConsignment.java
+++ Turquaz/com/turquaz/bill/ui/BillUIBillFromConsignment.java
@@ -17,7 +17,7 @@ package com.turquaz.bill.ui;
 /************************************************************************/
 /**
  * @author  Onsel Armagan
- * @version  $Id: BillUIBillFromConsignment.java,v 1.46 2005/04/24 14:53:57 cemdayanik Exp $
+ * @version  $Id: BillUIBillFromConsignment.java,v 1.47 2005/04/25 11:54:30 onsel Exp $
  */
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -26,7 +26,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
-import org.apache.log4j.Logger;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.layout.GridData;
@@ -62,6 +61,7 @@ import com.turquaz.consignment.bl.ConBLUpdateConsignment;
 import com.turquaz.consignment.ui.ConUIConsignmentSearchDialog;
 import com.turquaz.engine.bl.EngBLCommon;
 import com.turquaz.engine.bl.EngBLHibernateComparer;
+import com.turquaz.engine.bl.EngBLLogger;
 import com.turquaz.engine.bl.EngBLUtils;
 import com.turquaz.engine.dal.TurqBill;
 import com.turquaz.engine.dal.TurqBillGroup;
@@ -605,7 +605,7 @@ public class BillUIBillFromConsignment extends org.eclipse.swt.widgets.Composite
 		}
 		catch (Exception e)
 		{
-			e.printStackTrace();
+            EngBLLogger.log(this.getClass(),e,getShell());
 		}
 	}
 
@@ -632,9 +632,7 @@ public class BillUIBillFromConsignment extends org.eclipse.swt.widgets.Composite
 		}
 		catch (Exception ex)
 		{
-			Logger loger = Logger.getLogger(this.getClass());
-			loger.error("Exception Caught", ex);
-			ex.printStackTrace();
+            EngBLLogger.log(this.getClass(),ex,getShell());
 		}
 	}
 	public void calculateTotals()
@@ -732,11 +730,7 @@ public class BillUIBillFromConsignment extends org.eclipse.swt.widgets.Composite
 		}
 		catch (Exception ex)
 		{
-			Logger loger = Logger.getLogger(this.getClass());
-			loger.error("Exception Caught", ex);
-			ex.printStackTrace();
-			msg.setMessage(ex.getMessage());
-			msg.open();
+            EngBLLogger.log(this.getClass(),ex,getShell());
 		}
 	}
 
@@ -844,10 +838,7 @@ public class BillUIBillFromConsignment extends org.eclipse.swt.widgets.Composite
 			}
 			catch (Exception ex)
 			{
-				Logger loger = Logger.getLogger(this.getClass());
-				loger.error("Exception Caught", ex);
-				ex.printStackTrace();
-				EngUICommon.showMessageBox(getShell(), ex.getMessage(), SWT.ICON_ERROR);
+                EngBLLogger.log(this.getClass(),ex,getShell());
 			}
 		}
 		
