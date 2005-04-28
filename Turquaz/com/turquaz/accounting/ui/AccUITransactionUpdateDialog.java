9a50209b8f71513fbeb42fe73f2965b68397bfc0
cemdayanik <cemdayanik>
2005-04-28 17:16:28 +0000

    AccTransColumn Definitions are fixed..

--- Turquaz/com/turquaz/accounting/ui/AccUITransactionUpdateDialog.java
+++ Turquaz/com/turquaz/accounting/ui/AccUITransactionUpdateDialog.java
@@ -17,14 +17,13 @@ package com.turquaz.accounting.ui;
 /************************************************************************/
 /**
  * @author  Onsel Armagan
- * @version  $Id: AccUITransactionUpdateDialog.java,v 1.47 2005/04/28 10:02:12 onsel Exp $
+ * @version  $Id: AccUITransactionUpdateDialog.java,v 1.48 2005/04/28 17:16:31 cemdayanik Exp $
  */
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
@@ -292,9 +291,6 @@ public class AccUITransactionUpdateDialog extends org.eclipse.swt.widgets.Dialog
 			if (compTransactionAdd.verifyFields())
 			{
 				updated = true;
-				Map creditAccounts = new HashMap();
-				Map deptAccounts = new HashMap();
-				compTransactionAdd.prepareAccountingMaps(creditAccounts, deptAccounts);
 				
 				HashMap argMap = new HashMap();
 				argMap.put(AccKeys.ACC_TRANSACTION,accTrans);
@@ -302,9 +298,7 @@ public class AccUITransactionUpdateDialog extends org.eclipse.swt.widgets.Dialog
 				argMap.put(AccKeys.ACC_TRANS_DATE, compTransactionAdd.getDateTransactionDate().getDate());
 				argMap.put(AccKeys.ACC_DEFINITION,compTransactionAdd.getTxtTransDefinition().getText().trim());
 				argMap.put(EngKeys.EXCHANGE_RATE,EngBLCommon.getBaseCurrencyExchangeRate());
-				argMap.put(AccKeys.ACC_DEPT_ACCOUNT_MAP,deptAccounts);
-				argMap.put(AccKeys.ACC_CREDIT_ACCOUNT_MAP,creditAccounts);
-				argMap.put(AccKeys.ACC_SUM_ROWS,new Boolean(false));
+				argMap.put(AccKeys.ACC_TRANSACTIONS,compTransactionAdd.getTransactionColumns());
 				
 				EngTXCommon.doTransactionTX(AccBLTransactionUpdate.class.getName(),"updateTransaction",argMap);
 				
