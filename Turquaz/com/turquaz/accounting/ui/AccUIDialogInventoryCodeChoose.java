468b641d81a5892a2da03b37345954c8b377c97f
huseyiner <huseyiner>
2005-04-30 13:49:38 +0000

    language cleaning cont.

--- Turquaz/com/turquaz/accounting/ui/AccUIDialogInventoryCodeChoose.java
+++ Turquaz/com/turquaz/accounting/ui/AccUIDialogInventoryCodeChoose.java
@@ -17,7 +17,7 @@ package com.turquaz.accounting.ui;
 /************************************************************************/
 /**
  * @author  Onsel Armagan
- * @version  $Id: AccUIDialogInventoryCodeChoose.java,v 1.14 2005/04/28 10:02:12 onsel Exp $
+ * @version  $Id: AccUIDialogInventoryCodeChoose.java,v 1.15 2005/04/30 13:49:39 huseyiner Exp $
  */
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.graphics.Point;
@@ -32,20 +32,7 @@ import org.eclipse.swt.events.MouseAdapter;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.widgets.Tree;
 
-/**
-* This code was generated using CloudGarden's Jigloo
-* SWT/Swing GUI Builder, which is free for non-commercial
-* use. If Jigloo is being used commercially (ie, by a corporation,
-* company or business for any purpose whatever) then you
-* should purchase a license for each developer using Jigloo.
-* Please visit www.cloudgarden.com for details.
-* Use of Jigloo implies acceptance of these licensing terms.
-* *************************************
-* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED
-* for this machine, so Jigloo or this code cannot be used legally
-* for any corporate or commercial purpose.
-* *************************************
-*/
+
 public class AccUIDialogInventoryCodeChoose extends org.eclipse.swt.widgets.Dialog
 {
 	private Shell dialogShell;
