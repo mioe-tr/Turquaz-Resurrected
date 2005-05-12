611936cab507d1cc651380c7322c2362d44fe6bb
onsel <onsel>
2005-05-12 14:05:56 +0000

    bug fixed in update bill dialogs.

--- Turquaz/com/turquaz/bill/ui/BillUIAddBill.java
+++ Turquaz/com/turquaz/bill/ui/BillUIAddBill.java
@@ -17,7 +17,7 @@ package com.turquaz.bill.ui;
 /************************************************************************/
 /**
  * @author  Onsel Armagan
- * @version  $Id: BillUIAddBill.java,v 1.85 2005/05/09 12:44:39 onsel Exp $
+ * @version  $Id: BillUIAddBill.java,v 1.86 2005/05/12 14:05:56 onsel Exp $
  */
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -65,6 +65,7 @@ import com.turquaz.engine.dal.TurqViewInventoryAmountTotal;
 import com.turquaz.engine.dal.TurqInventoryTransaction;
 import com.turquaz.engine.interfaces.SecureComposite;
 import com.turquaz.engine.lang.BillLangKeys;
+import com.turquaz.engine.lang.CashLangKeys;
 import com.turquaz.engine.lang.ConsLangKeys;
 import com.turquaz.engine.lang.CurLangKeys;
 import com.turquaz.engine.lang.EngLangCommonKeys;
@@ -912,6 +913,15 @@ public class BillUIAddBill extends Composite implements SecureComposite
 			cursor.setFocus();
 			return false;
 		}
+         if(btnClosedBill.getSelection())
+        {
+            if(cashPicker.getTurqCashCard()==null)
+            {
+                EngUICommon.showMessageBox(getShell(),CashLangKeys.MSG_SELECT_CASH_CARD,SWT.ICON_WARNING);
+                cashPicker.setFocus();
+                return false;
+            }
+        }
 		boolean isExistEntry = false;
 		TableItem items[] = tableConsignmentRows.getItems();
 		for (int k = 0; k < items.length; k++)
