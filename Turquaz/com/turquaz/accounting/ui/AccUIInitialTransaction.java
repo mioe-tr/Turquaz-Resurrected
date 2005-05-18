ff409c71df6ee8bfd23e41aca7aae61ae48790c0
cemdayanik <cemdayanik>
2005-05-18 15:44:14 +0000

    *** empty log message ***

--- Turquaz/com/turquaz/accounting/ui/AccUIInitialTransaction.java
+++ Turquaz/com/turquaz/accounting/ui/AccUIInitialTransaction.java
@@ -17,7 +17,7 @@ package com.turquaz.accounting.ui;
 /************************************************************************/
 /**
  * @author  Onsel Armagan
- * @version  $Id: AccUIInitialTransaction.java,v 1.33 2005/04/30 13:49:38 huseyiner Exp $
+ * @version  $Id: AccUIInitialTransaction.java,v 1.34 2005/05/18 15:48:29 cemdayanik Exp $
  */
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -75,6 +75,21 @@ import org.eclipse.swt.events.VerifyEvent;
 import com.cloudgarden.resource.SWTResourceManager;
 
 
+
+/**
+* This code was generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* *************************************
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED
+* for this machine, so Jigloo or this code cannot be used legally
+* for any corporate or commercial purpose.
+* *************************************
+*/
 public class AccUIInitialTransaction extends Composite implements SecureComposite
 {
 	{
