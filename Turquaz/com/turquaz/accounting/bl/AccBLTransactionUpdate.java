780dc41010c80172466e6320a0032966c26a0d13
cemdayanik <cemdayanik>
2005-05-31 16:33:46 +0000

    *** empty log message ***

--- Turquaz/com/turquaz/accounting/bl/AccBLTransactionUpdate.java
+++ Turquaz/com/turquaz/accounting/bl/AccBLTransactionUpdate.java
@@ -17,7 +17,7 @@ package com.turquaz.accounting.bl;
 /************************************************************************/
 /**
  * @author Onsel Armagan
- * @version $Id: AccBLTransactionUpdate.java,v 1.21 2005/05/30 08:52:43 cemdayanik Exp $
+ * @version $Id: AccBLTransactionUpdate.java,v 1.22 2005/05/31 16:33:46 cemdayanik Exp $
  */
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -96,7 +96,7 @@ public class AccBLTransactionUpdate
 			transBag.put(AccKeys.ACC_TRANS_ID,trans.getId());
 			transBag.put(AccKeys.ACC_TRANSACTION_DEFINITION,trans.getTransactionDescription());
 			transBag.put(AccKeys.ACC_TRANSACTION_DOC_NO,trans.getTransactionDocumentNo());
-			transBag.put(AccKeys.ACC_TRANSACTION_DATE,trans.getTransactionsDate());
+			transBag.put(AccKeys.ACC_TRANS_DATE,trans.getTransactionsDate());
 			transBag.put(EngKeys.EXCHANGE_CURRENCY_ID,trans.getTurqCurrencyExchangeRate().getId());
 			transBag.put(EngKeys.EXCHANGE_CURRENCY_ABBR,trans.getTurqCurrencyExchangeRate().getTurqCurrencyByExchangeCurrencyId().getCurrenciesAbbreviation());
 			
--- Turquaz/com/turquaz/accounting/ui/AccUIInitialTransaction.java
+++ Turquaz/com/turquaz/accounting/ui/AccUIInitialTransaction.java
@@ -17,7 +17,7 @@ package com.turquaz.accounting.ui;
 /************************************************************************/
 /**
  * @author  Onsel Armagan
- * @version  $Id: AccUIInitialTransaction.java,v 1.35 2005/05/30 08:52:43 cemdayanik Exp $
+ * @version  $Id: AccUIInitialTransaction.java,v 1.36 2005/05/31 16:33:47 cemdayanik Exp $
  */
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -328,7 +328,7 @@ public class AccUIInitialTransaction extends Composite implements SecureComposit
 			{
 				txtDocumentNo.setText((String)accountBag.get(AccKeys.ACC_TRANSACTION_DOC_NO));
 				txtTransDefinition.setText((String)accountBag.get(AccKeys.ACC_TRANSACTION_DEFINITION));
-				dateTransactionDate.setDate((Date)accountBag.get(AccKeys.ACC_TRANSACTION_DATE));
+				dateTransactionDate.setDate((Date)accountBag.get(AccKeys.ACC_TRANS_DATE));
 
 				HashMap transRows=(HashMap)accountBag.get(AccKeys.ACC_TRANSACTION_ROWS);
 
